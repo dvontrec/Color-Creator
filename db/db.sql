ae22f412995482c963a457815617a966bf022774
Dvontre Coleman <dvontrec@gmail.com>
2019-02-09 21:30:01 -0500

    Update DB seed data

--- db/db.sql
+++ db/db.sql
@@ -4,20 +4,30 @@ DROP TABLE IF EXISTS colors;
 CREATE TABLE colors
 (
   color VARCHAR(30),
-  r VARCHAR(3) DEFAULT "0",
-  g VARCHAR(3) DEFAULT "0",
-  b VARCHAR(3) DEFAULT "0",
-  a VARCHAR(3) DEFAULT "0",
+
+  r TINYINT(1)
+  UNSIGNED DEFAULT 0 ,
+
+  g TINYINT
+  (1) UNSIGNED  DEFAULT 0 ,
+
+  b TINYINT
+  (1)  UNSIGNED DEFAULT 0 ,
+
+  a TINYINT
+  (1)  UNSIGNED DEFAULT 255 ,
+  
   views INT DEFAULT 0
 );
 
--- Insert red, blue, and yellow into the db
-INSERT INTO colors
-  (color)
-VALUES("red");
-INSERT INTO colors
-  (color)
-VALUES("blue");
-INSERT INTO colors
-  (color)
-VALUES("yellow");
+
+  -- Insert red, blue, and yellow into the db
+  INSERT INTO colors
+    (color,r)
+  VALUES("red", 255);
+  INSERT INTO colors
+    (color,b)
+  VALUES("blue", 255);
+  INSERT INTO colors
+    (color,g)
+  VALUES("green", 255);
