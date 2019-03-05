8e3e7015de986d2ede927d1fbe30393450142b2a
Dvontre Coleman <dvontrec@gmail.com>
2019-03-05 09:40:54 -0500

    Update color return order now shows by green descending

--- api/color_routes.go
+++ api/color_routes.go
@@ -32,7 +32,7 @@ func getColors(w http.ResponseWriter) {
 	var colors []color
 
 	// runs a query to pull data from the database
-	rows, err := db.Query(`SELECT color, r, g, b, a, hex, creatorId, creatorHash FROM colors;`)
+	rows, err := db.Query(`SELECT color, r, g, b, a, hex, creatorId, creatorHash FROM colors ORDER BY g ASC;`)
 	check(err)
 
 	var name, r, g, b, a, hex, cId, cH string
