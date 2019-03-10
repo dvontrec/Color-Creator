c4587166f5514c4723a85adf2883c18443af22ae
Dvontre Coleman <dvontrec@gmail.com>
2019-03-09 19:07:54 -0500

    Add profile skeleton to client

---
+++ client/src/components/Users/Profile.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Profile extends Component {
+  render() {
+    return <div>Profile</div>;
+  }
+}
+export default connect(null)(Profile);
