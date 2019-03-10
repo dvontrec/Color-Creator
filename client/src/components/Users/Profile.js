454f6657876e87a94316fee9d3adc6e177f8d084
Dvontre Coleman <dvontrec@gmail.com>
2019-03-10 11:48:58 -0400

    Begin code for user profile display

--- client/src/components/Users/Profile.js
+++ client/src/components/Users/Profile.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import api from '../../apis/colors';
 
 class Profile extends Component {
+  async componentDidMount() {
+    // saves the response from the api call to be the response data
+    const response = await api.get(`api/user?userId=2`);
+    console.log(response.data);
+  }
   render() {
     return <div className="container">Profile</div>;
   }
 }
-export default connect(null)(Profile);
+export default Profile;
