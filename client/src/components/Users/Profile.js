0c9b6501829b68851236e4e179d16de8dd231716
Dvontre Coleman <dvontrec@gmail.com>
2019-03-24 12:15:22 -0400

    Refactor Profile color view

--- client/src/components/Users/Profile.js
+++ client/src/components/Users/Profile.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
-import style from '../../style.css';
 import api from '../../apis/colors';
 import ColorContainer from '../Colors/ColorContainer';
 
@@ -13,41 +11,22 @@ class Profile extends Component {
     );
     this.setState({ UserData: response.data });
   }
-  renderColor = color => {
-    if (color) {
-      const colorCode = `rgba(${color.r},${color.g},${color.b},${color.a})`;
-      return (
-        <div key={color.hex} className="col-md-1">
-          <Link to={`/color/${color.hex}`}>
-            <div
-              className={style.colorBlock}
-              style={{ background: colorCode }}
-            />
-          </Link>
-        </div>
-      );
-    }
-    return <div>Loading Color</div>;
-  };
 
   renderProfile() {
     if (!this.state) {
       return <div>Retrieving User</div>;
     }
-    const userInfo = this.state.UserData.userInfo;
-    const favoriteColors = this.state.UserData.favoriteColors;
-    const createdColors = this.state.UserData.createdColors;
     return (
       <div>
-        <h4>{userInfo.username.toUpperCase()}</h4>
+        <h4>{this.state.UserData.userInfo.username.toUpperCase()}</h4>
         <h4>Created Colors</h4>
         <div className="row">
-          <ColorContainer color={createdColors} />
+          <ColorContainer color={this.state.UserData.createdColors} />
         </div>
         <hr />
         <h4>Favorite Colors</h4>
         <div className="row">
-          <ColorContainer color={favoriteColors} />
+          <ColorContainer color={this.state.UserData.favoriteColors} />
         </div>
       </div>
     );
