59fd0346f007b638d0b9002af953193e6cc26062
Dvontre Coleman <dvontrec@gmail.com>
2019-04-13 14:30:50 -0400

    Add create palette for api

--- api/palette_route.go
+++ api/palette_route.go
@@ -1,13 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-// Function used to get pallette by the request user or color
-func getPallette(w http.ResponseWriter, req *http.Request) {
-	// This function takes data from the request
-	// If the request has a user it grabs the palettes created by the user
-	// If the request has a hex provided it grabs palettes that use that color
-	// The palette is saved to an array
-	// The array is passed as json
+func palette(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		createPalette(w, req)
+		return
+	}
+}
+
+// function used to create palette
+func createPalette(w http.ResponseWriter, req *http.Request) {
+	// Gets the userID, paletteName, P, S, and T hex from the request body
+	userID := req.FormValue("userID")
+	paletteName := req.FormValue("paletteName")
+	primaryHex := req.FormValue("primaryHex")
+	secondaryHex := req.FormValue("secondaryHex")
+	tertiaryHex := req.FormValue("tertiaryHex")
+	// created a query to create the palette
+	q := fmt.Sprintf("INSERT INTO palettes(creatorID, paletteName, primaryHex, secondaryHex, tertiaryHex) VALUES(%v, %v, %v, %v, %v);", userID, paletteName, primaryHex, secondaryHex, tertiaryHex)
+	fmt.Println(q)
+	// // Prepares the query
+	// stmt, err := db.Prepare(q)
+	// // checks the error
+	// htmlCheck(err, w, fmt.Sprint("There was an error ", err))
+
+	// r, err := stmt.Exec()
+	// // checks the error
+	// htmlCheck(err, w, fmt.Sprint("There was an error ", err))
+
+	// _, err = r.RowsAffected()
+	// // checks the error
+	// htmlCheck(err, w, fmt.Sprint("There was an error ", err))
+	// // writes the status was created
+	// w.WriteHeader(http.StatusCreated)
+	// fmt.Fprintf(w, "palette created")
+	// // checks the error
+	// htmlCheck(err, w, fmt.Sprint("There was an error ", err))
 
 }
