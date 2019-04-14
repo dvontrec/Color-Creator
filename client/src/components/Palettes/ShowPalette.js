6837cde5f56394d1cf7f7bb9ae910301c6332638
Dvontre Coleman <dvontrec@gmail.com>
2019-04-14 19:47:46 -0400

    Add palette reducer

--- client/src/components/Palettes/ShowPalette.js
+++ client/src/components/Palettes/ShowPalette.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { getPalette } from '../../actions';
 import api from '../../apis/colors';
 
 class ShowPalette extends Component {
   state = {};
   async componentDidMount() {
-    // calls the api passing in the id parameter from the route
-    const response = await api.get(
-      `/api/palettes?paletteID=${this.props.match.params.id}`
-    );
-    this.setState({ paletteData: response.data });
+    this.props.getPalette(this.props.match.params.id);
   }
 
   renderColor(color) {
@@ -37,6 +36,7 @@ class ShowPalette extends Component {
   }
 
   renderPalette() {
+    console.log(this.props);
     if (this.state.paletteData) {
       return (
         <div>
@@ -46,15 +46,56 @@ class ShowPalette extends Component {
           <div>{this.renderColor(this.state.paletteData.Secondary)}</div>
           <hr />
           <div>{this.renderColor(this.state.paletteData.Tertiary)}</div>
+          <Link className="btn btn-primary" to={`/profile/`}>
+            View Creator Profile
+          </Link>
+          {/* {this.checkFavorite()} */}
         </div>
       );
     }
     return <div>Loading...</div>;
   }
 
+  addFavorite = () => {
+    const favs = this.props.favorites || this.state.favorites;
+    favs.push(this.props.auth.userId);
+    const favoritesQuery = `userId=${this.props.auth.userId}&userHash=${
+      this.props.auth.userHash
+    }&colorHex=${this.props.color.hex}`;
+    this.props.addFavorites(favoritesQuery);
+    this.setState({ favorites: favs });
+  };
+
+  removeFavorite = () => {
+    const newFavs =
+      this.props.favorites.filter(e => e != this.props.auth.userId) ||
+      this.props.favorites ||
+      this.state.favorites.filter(e => e != this.props.auth.userId);
+    const favoritesQuery = `userId=${this.props.auth.userId}&userHash=${
+      this.props.auth.userHash
+    }&colorHex=${this.props.color.hex}`;
+    this.props.removeFavorites(favoritesQuery);
+    this.setState({ favorites: newFavs });
+  };
+
+  checkFavorite = () => {
+    if (this.props.auth.userId) {
+      const favArray = this.state.favorites || this.props.favorites;
+      if (favArray && favArray.includes(this.props.auth.userId.toString())) {
+        return (
+          <button onClick={this.removeFavorite}>Remove From Favorites</button>
+        );
+      }
+      return <button onClick={this.addFavorite}>Add To Favorite</button>;
+    }
+  };
+
   render() {
     return <div className="container">{this.renderPalette()}</div>;
   }
 }
 
-export default ShowPalette;
+export default connect(
+  null,
+  { getPalette }
+)(ShowPalette);
